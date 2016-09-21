248365621a55a44eb975cd8ab7947bb905f67a26
kmurray <k.murray@utoronto.ca>
2016-09-21 13:16:37 -0400

    Improve output formatting

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char **argv) {
 
 
     //Print out the commands
-    for(const auto sdc_create_clock : sdc_commands->create_clock_cmds) {
+    for(const auto& sdc_create_clock : sdc_commands->create_clock_cmds) {
         printf("Line %3d: ", sdc_create_clock.file_line_number);
         if(sdc_create_clock.is_virtual) {
             printf("create_clock -period %f -waveform {%f %f} -name %s\n",
@@ -47,7 +47,7 @@ int main(int argc, char **argv) {
         }
     }
 
-    for(const auto sdc_set_input_delay : sdc_commands->set_input_delay_cmds) {
+    for(const auto& sdc_set_input_delay : sdc_commands->set_input_delay_cmds) {
         printf("Line %3d: ", sdc_set_input_delay.file_line_number);
         printf("set_input_delay -clock %s -max %f ", 
                     sdc_set_input_delay.clock_name.c_str(),
@@ -56,7 +56,7 @@ int main(int argc, char **argv) {
         printf("\n");
     }
 
-    for(const auto sdc_set_output_delay : sdc_commands->set_output_delay_cmds) {
+    for(const auto& sdc_set_output_delay : sdc_commands->set_output_delay_cmds) {
         printf("Line %3d: ", sdc_set_output_delay.file_line_number);
         printf("set_output_delay -clock %s -max %f ", 
                     sdc_set_output_delay.clock_name.c_str(),
@@ -65,7 +65,7 @@ int main(int argc, char **argv) {
         printf("\n");
     }
 
-    for(const auto sdc_set_clock_groups : sdc_commands->set_clock_groups_cmds) {
+    for(const auto& sdc_set_clock_groups : sdc_commands->set_clock_groups_cmds) {
         printf("Line %3d: ", sdc_set_clock_groups.file_line_number);
         printf("set_clock_groups ");
         if(sdc_set_clock_groups.type == ClockGroupsType::EXCLUSIVE) {
@@ -78,21 +78,21 @@ int main(int argc, char **argv) {
         printf("\n");
     }
 
-    for(const auto sdc_set_false_path : sdc_commands->set_false_path_cmds) {
+    for(const auto& sdc_set_false_path : sdc_commands->set_false_path_cmds) {
         printf("Line %3d: ", sdc_set_false_path.file_line_number);
         printf("set_false_path ");
         print_from_to_group(sdc_set_false_path.from, sdc_set_false_path.to);
         printf("\n");
     }
 
-    for(const auto sdc_set_max_delay : sdc_commands->set_max_delay_cmds) {
+    for(const auto& sdc_set_max_delay : sdc_commands->set_max_delay_cmds) {
         printf("Line %3d: ", sdc_set_max_delay.file_line_number);
         printf("set_max_delay %f ", sdc_set_max_delay.max_delay);
         print_from_to_group(sdc_set_max_delay.from, sdc_set_max_delay.to);
         printf("\n");
     }
 
-    for(const auto sdc_set_multicycle_path : sdc_commands->set_multicycle_path_cmds) {
+    for(const auto& sdc_set_multicycle_path : sdc_commands->set_multicycle_path_cmds) {
         printf("Line %3d: ", sdc_set_multicycle_path.file_line_number);
         printf("set_multicycle_path %d ", sdc_set_multicycle_path.mcp_value);
         if(sdc_set_multicycle_path.type == McpType::SETUP) {
@@ -125,8 +125,12 @@ void print_string_group(const StringGroup& group) {
     }
 
     printf("%s", start_token);
-    for(const auto& string : group.strings) {
-        printf("%s ", string.c_str());
+    for(size_t i = 0; i < group.strings.size(); ++i) {
+        printf("%s", group.strings[i].c_str());
+
+        if(i != group.strings.size() - 1) {
+            printf(" ");
+        }
     }
     printf("%s", end_token);
 }
