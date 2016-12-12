6e6154da12636f9640b240e036df83a2fba16b4f
kmurray <k.murray@utoronto.ca>
2016-12-12 11:39:51 -0500

    Update tests

--- test_sdcs/test_parser.sh
+++ test_sdcs/test_parser.sh
@@ -10,10 +10,23 @@ do
         #valgrind --leak-check=full --track-origins=yes ./sdcparse_test $sdc_file
         ./sdcparse_test $sdc_file
         exit_code=$?
-        if [ $exit_code -ne 0 ]; then
-            echo "Error"
-            exit 1
+        if [[ $sdc_file != *"invalid"* ]]; then
+            if [ $exit_code -ne 0 ]; then
+                    #Should have parsed ok
+                    echo "Error: $sdc_file should have parsed correctly!"
+                    exit 1
+            fi
+        else
+            if [ $exit_code -eq 0 ]; then
+                echo "Error: $sdc_file should have failed to parse!"
+                exit 1
+            else
+                echo "PASS: invalid sdc $sdc_file did not parse (as expected)"
+            fi
         fi
     done
 
 done
+
+echo "ALL TESTS PASSED"
+exit 0
