717222b296b7275b633948f8cd03701fe61ad3dd
oscarcheng105 <oscarcheng105@gmail.com>
2026-04-23 23:27:09 +0800

    1) revert to original vtr version to enable tcl and swig 2) Add NOT MSVC guards for -Wno-cast-qual -Wno-shadow

--- src/sdcparse.cpp
+++ src/sdcparse.cpp
@@ -1,20 +1,12 @@
 #include "sdcparse.hpp"
 
-#if LIBSDCPARSE_WITH_TCL
 #include "tcl_interpreter.h"
-#endif
 
 namespace sdcparse {
 
 void sdc_parse_filename(const std::string& filename, Callback& callback) {
-#if LIBSDCPARSE_WITH_TCL
     TclInterpreter interpreter(callback);
     interpreter.eval_file(filename);
-#else
-    (void)filename;
-    (void)callback;
-    throw std::runtime_error("libsdcparse built without TCL support");
-#endif
 }
 
-} // namespace sdcparse
+}
