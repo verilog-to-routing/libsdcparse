e5c2748550ebd880ec210acb1c4a797fa044362b
oscarcheng105 <oscarcheng105@gmail.com>
2026-04-23 08:40:57 +0800

    Add optional TCL support (VTR)

--- src/sdcparse.cpp
+++ src/sdcparse.cpp
@@ -1,12 +1,20 @@
 #include "sdcparse.hpp"
 
+#if LIBSDCPARSE_WITH_TCL
 #include "tcl_interpreter.h"
+#endif
 
 namespace sdcparse {
 
 void sdc_parse_filename(const std::string& filename, Callback& callback) {
+#if LIBSDCPARSE_WITH_TCL
     TclInterpreter interpreter(callback);
     interpreter.eval_file(filename);
+#else
+    (void)filename;
+    (void)callback;
+    throw std::runtime_error("libsdcparse built without TCL support");
+#endif
 }
 
-}
+} // namespace sdcparse
